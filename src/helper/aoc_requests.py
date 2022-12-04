15bf6842cbbd7c48ab7a905c1c602e819515c232
Jace Iverson <jiverson@Jace-Iverson-DC6V2M5FWK.local>
2022-12-04 01:13:15 -0500

    created separate requests module to standardize User-Agent in requests

---
+++ src/helper/aoc_requests.py
@@ -0,0 +1,26 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def get_aoc_page(url: str) -> requests.Response:
+    """
+    Makes HTTP requests to the advent of code website
+    Pulls in the COOKIE_SESSION enviornment variable
+    and standardized User-Agent as was requested
+
+    Args:
+        url (str): url that we will pull
+
+    Returns:
+        requests.Response: object response
+    """
+    return requests.get(
+        url=url,
+        cookies={"session": os.environ["COOKIE_SESSION"]},
+        headers={
+            "User-agent": "github.com/jaceiverson/aoc-util by iverson.jace@gmail.com"
+        },
+    )
